472ca24daa697641837e2604ae4d92d3394673e1
Dishalex <dishalex@gmail.com>
2023-11-14 00:40:42 +0200

    upgrade

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'noteapp'
 urlpatterns = [
     path('', views.main, name='main'),
     path('tag/', views.tag, name='tag'),
+    path('note/', views.note, name='note'),
+    path('detail/<int:note_id>', views.detail, name='detail'),
 ]
